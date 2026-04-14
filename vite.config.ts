65b72b1c4cfcf9fabf04c405dcee7aa846b4a39e
minhnb0503 <minh.nb0503@gmail.com>
2026-04-15 01:32:07 +0700

    ádasd

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    hmr: {
+      overlay: false // ĐÂY LÀ LỆNH TẮT CÁI MÀNG VÔ HÌNH CHẶN CLICK
+    }
+  }
 })
